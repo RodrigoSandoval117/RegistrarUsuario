2caf3269d517f144d581676ea112e1eb9611c1b5
RodrigoSandoval117 <RodrigoSandoval117@users.noreply.github.com>
2020-07-16 23:47:28 -0500

    Cambio de IP

--- RodrigoSandoval/RegistroUsuarioRodrigoSandoval/DAO/Conexion.cs
+++ RodrigoSandoval/RegistroUsuarioRodrigoSandoval/DAO/Conexion.cs
@@ -10,7 +10,7 @@ namespace RegistroUsuarioRodrigoSandoval.DAO
     {
         public SqlConnection ObtenerConexion()
         {
-            return new SqlConnection(@"data source = 104.198.196.57; initial catalog = RegistroUsuario; user id = sqlserver; password = 123456");
+            return new SqlConnection(@"data source = 34.71.231.215; initial catalog = RegistroUsuario; user id = sqlserver; password = 123456");
         }
     }
 }
